611481f6905985b5ec371194c4adcedc8b2f27d5
Hariprajaa05 <953622104030@ritrjpm.ac.in>
2024-10-15 22:54:48 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,9 +1,19 @@
-export const emailValidator = email => {
-    const emailRegex = /^[^\s@]+@[^\s@]+$/;
-    return emailRegex.test(email)
+//make components like the Login and import it here thats it. 
+
+/* Frontend (React) will fetch data from the Backend (Node.js + Express).
+The Backend will interact with the Database (MongoDB) to get or store information.*/
+
+import './App.css';
+import Login from './components/Login.jsx';
+import Signup from './components/signup.jsx';
+
+function App() {
+  return (
+    <div>
+      <Signup />
+      <Login />
+    </div>
+  );
 }
 
-export const passwordValidator = password => {
-    const passwordRegex = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/;
-    return passwordRegex.test(password)
-}
+export default App;
